91f4d993eeefc946c26621214e960cffd9b07c75
Christian Glombek <LorbusChris@users.noreply.github.com>
2016-03-21 02:37:44 +0100

    Update smoketest.js

--- source/test/functional/browser/smoketest.js
+++ source/test/functional/browser/smoketest.js
@@ -1,12 +1,11 @@
-const WAIT = 20000;
+const WAIT = 3000;
 const NODE_PORT = process.env.NODE_PORT || 3000;
 
 module.exports = {
   'Smoketest' (browser) {
     browser.url(`http://localhost:${NODE_PORT}/`);
     browser.waitForElementVisible('body', WAIT);
-    browser.click('body');
     browser.expect.element('body').text.to.contain('Client render').before(WAIT);
     browser.end();
-      }
+  }
 };
