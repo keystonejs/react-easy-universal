9804989f5189035343ddb7761413baca4bb2c2a1
LorbusChris <c.glombek@t-online.de>
2016-03-23 16:47:53 +0100

    add launch_url to nightwatch, avoid hardcoding

--- source/test/functional/browser/smoketest.js
+++ source/test/functional/browser/smoketest.js
@@ -3,7 +3,7 @@ const NODE_PORT = process.env.NODE_PORT || 3000;
 
 module.exports = {
   'Smoketest' (browser) {
-    browser.url(`http://localhost:${NODE_PORT}/`);
+    browser.url(`${browser.launchUrl}:${NODE_PORT}/`);
     browser.waitForElementVisible('body', WAIT);
     browser.expect.element('body').text.to.contain('Client render').before(WAIT);
     browser.end();
