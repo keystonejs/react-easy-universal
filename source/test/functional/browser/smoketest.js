f62fe7af082fc722c031accf94551b06e23a096b
LorbusChris <c.glombek@t-online.de>
2016-03-24 01:10:35 +0100

    use nightwatch --env, bump nightwatch-autorun version, avoid hardcoding port in tests
    Use variable NIGHTWATCH_ENV to allow multiple envs be stored in one nightwatch config file.

--- source/test/functional/browser/smoketest.js
+++ source/test/functional/browser/smoketest.js
@@ -1,9 +1,8 @@
 const WAIT = 3000;
-const NODE_PORT = process.env.NODE_PORT || 3000;
 
 module.exports = {
   'Smoketest' (browser) {
-    browser.url(`${browser.launchUrl}:${NODE_PORT}/`);
+    browser.url(`${browser.launchUrl}/`);
     browser.waitForElementVisible('body', WAIT);
     browser.expect.element('body').text.to.contain('Client render').before(WAIT);
     browser.end();
